154ba9e959746efa7e78b0fe4d2ed5c90dd2a1ad
stevennoble78 <stevennoble78@hotmail.com>
2016-01-22 09:51:59 -0800

    initial commit, connected routes

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-var app = angular.module('instagramSearchApp', []);
+var app = angular.module('instagramSearchApp', ['ngRoute']);
+
+// Configuration
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/search.html',
+      controller: 'SearchCtrl'
+    })
+    .when('/favorites', {
+      templateUrl: 'templates/favorites.html',
+      controller: 'FavoritesCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+}]);
+
+// Controllers
+
+app.controller('SearchCtrl', ['$scope', '$http', function ($scope, $http) {
+  $scope.searchCtrlTest = 'search controller is working';
+}]);
+
+app.controller('FavoritesCtrl', ['$scope', function ($scope) {
+  $scope.favoritesCtrlTest = 'favorite controller is working';
+}]);

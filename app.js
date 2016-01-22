66314e424281250c2be20925e27e0dd9a7ca7b8c
abeytaadam <measalion@gmail.com>
2016-01-22 09:50:03 -0800

    Instagram initial setup

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-var app = angular.module('instagramSearchApp', []);
+var app = angular.module('instagramSearchApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/search.html',
+      controller: 'SearchCtrl'
+    })
+    .when('/favorites', {
+      templateUrl: 'templates/favorites.html',
+      controller: 'FavoritesCtrl'
+    });
+
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+}]);
+
+app.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.searchCtrlTest = "Hey it's working!";
+}]);
+
+app.controller('FavoritesCtrl', ['$scope', function($scope){
+	$scope.favoritesCtrlTest = "Hey this is working too!";
+}]);

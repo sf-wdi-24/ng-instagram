91ef31adff437e625c87960e44f850d21439c0ef
Cynthia Salinas <csalinas123@gmail.com>
2016-01-22 11:07:21 -0800

    add search

--- app.js
+++ app.js
@@ -26,6 +26,21 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 
 app.controller('SearchCtrl', ['$scope', '$http', function ($scope, $http) {
   $scope.searchCtrlTest = 'search controller is working';
+  $scope.searchTag = function (){
+    var tag = $scope.hashtag; 
+    var url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=d8d0d6b44249490bbde6eee4d1968dac&callback=JSON_CALLBACK';
+
+    $http.jsonp(url)
+      .then(function (response) {
+        $scope.hashtag = "";
+        console.log (response);
+        $scope.photos = response.data.data; 
+      }, function (error) {
+        // error callback
+      });
+  };
+ 
+
 }]);
 
 app.controller('FavoritesCtrl', ['$scope', function ($scope) {
